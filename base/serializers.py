7b6b37e1ac6352b2586b21590e47609818243fc5
Giorgi Baramidze <giorgi.baramidze0128@gmail.com>
2023-10-21 23:54:09 +0400

    director serializer

--- base/serializers.py
+++ base/serializers.py
@@ -1,7 +1,40 @@
 from rest_framework import serializers
-from .models import Movie
+from .models import Movie, Actor, Country, Director, Genre, Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = "__all__"
+        
+        
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = "__all__"
+        
+        
+class DirectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Director
+        fields = "__all__"
+        
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+        
+        
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = "__all__"
+
 
 class MovieSerializer(serializers.ModelSerializer):
+    director = DirectorSerializer(many=True)
+    genre = GenreSerializer(many=True)
+    country = CountrySerializer(many=True)
     
     class Meta:
         model = Movie
