1f270b9742c44b406abfbd448c510d805dbc5922
Giorgi Baramidze <giorgi.baramidze0128@gmail.com>
2023-10-24 00:05:14 +0400

    create user serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,27 @@
 from rest_framework import serializers
+from .models import Account
 
-class UserSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    password = serializers.CharField()
+
+class UserSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(style={'input_type': 'password'})
+    password2 = serializers.CharField(style={'input_type': 'password'})
+
+    class Meta:
+        model = Account
+        fields = ["email", "first_name", "last_name", "password1", "password2"]
+        
+    
+    def validate(self, data):
+        password1 = data.get('password1')
+        password2 = data.get('password2')
+        if password1 != password2:
+            raise serializers.ValidationError('Passwords do not match.')
+        return data
+
+    def create(self, validated_data):
+        user = Account.objects.create(email=validated_data['email'],
+                                       name=validated_data['name']
+                                         )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
