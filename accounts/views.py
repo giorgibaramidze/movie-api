08d9e5a9c69d380216f4eab753907b4e30c09d4b
Giorgi Baramidze <giorgi.baramidze0128@gmail.com>
2023-10-24 12:49:33 +0400

    remove unnecessary function

--- accounts/views.py
+++ accounts/views.py
@@ -13,35 +13,3 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({"message": "You have successfully registered"})
-
-
-class LoginAPI(APIView):
-    
-    def post(self, request):
-        data = request.data
-        serializer = UserSerializer(data=data)
-        if serializer.is_valid(raise_exception=True):
-            email = serializer.data['email']
-            password = serializer.data['password']
-            
-            user = authenticate(email=email, password=password)
-            
-            if user is None:
-                return Response({
-                'status': 400,
-                'message' : 'invalid password',
-                'data': {}
-                })
-
-            refresh = RefreshToken.for_user(user)
-            
-            return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-            })
-            
-        return Response({
-            'status': 400,
-            'message' : 'something went wrong',
-            'data': serializer.errors
-        })
